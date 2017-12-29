1205d25d1dd51235ca003ec8f87ebc33f782013a
haris <haris.math@gmail.com>
2017-12-29 12:06:32 +0500

    switch back camera to front camera

--- CameraPractice/ViewController.swift
+++ CameraPractice/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
     var captureSession = AVCaptureSession()
     var sessionOutput = AVCaptureStillImageOutput()
     var layer = AVCaptureVideoPreviewLayer()
+    var inputdevice  = AVCaptureDeviceInput()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,27 +27,39 @@ class ViewController: UIViewController {
         
     }
     
-    override func viewDidAppear(animated: Bool) {
+    @IBAction func frontCamera(sender: UIButton) {
+       self.getfrontCamera()
         
+    }
+    override func viewDidAppear(animated: Bool) {
+        self.getBackCamera()
+    }
+    @IBAction func backCamerapressed(sender: UIButton) {
+        self.getBackCamera()
+    }
+
+    func getfrontCamera(){
+        
+        captureSession.removeInput(self.inputdevice)
         let devices = AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo)
         
-        
         for device in devices{
-        
-            if device.position == .Back{
             
+            if device.position == .Front{
+                
                 do{
                     
-                    let input = try AVCaptureDeviceInput(device: device as! AVCaptureDevice)
+                    self.inputdevice = try AVCaptureDeviceInput(device: device as! AVCaptureDevice)
                     
-                    if captureSession.canAddInput(input){
                     
-                        captureSession.addInput(input)
+                    if captureSession.canAddInput(self.inputdevice){
+                        
+                        captureSession.addInput(self.inputdevice)
                         
                         sessionOutput.outputSettings = [AVVideoCodecKey:AVVideoCodecJPEG]
                         
                         if captureSession.canAddOutput(sessionOutput){
-                        
+                            
                             captureSession.addOutput(sessionOutput)
                             captureSession.startRunning()
                             
@@ -60,21 +73,72 @@ class ViewController: UIViewController {
                         }
                         
                     }
-                
+                    
                 }catch {
-                
-                
-                
+                    
+                    
+                    
                 }
-            
-            
+                
+                
             }
-        
+            
         }
-        
-    }
 
-   
+    }
+    
+    func getBackCamera(){
+        
+         captureSession.removeInput(self.inputdevice)
+        
+        let devices = AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo)
+        
+        
+        for device in devices{
+            
+            if device.position == .Back{
+                
+                do{
+                    
+                    self.inputdevice = try AVCaptureDeviceInput(device: device as! AVCaptureDevice)
+                    
+                    if captureSession.canAddInput(self.inputdevice){
+                        
+                        captureSession.addInput(self.inputdevice)
+                        
+                        sessionOutput.outputSettings = [AVVideoCodecKey:AVVideoCodecJPEG]
+                        
+                        if captureSession.canAddOutput(sessionOutput){
+                            
+                            captureSession.addOutput(sessionOutput)
+                            captureSession.startRunning()
+                            
+                            layer = AVCaptureVideoPreviewLayer(session: captureSession)
+                            layer.videoGravity = AVLayerVideoGravityResizeAspectFill
+                            layer.connection.videoOrientation = .Portrait
+                            self.cameraView.layer.addSublayer(layer)
+                            layer.position = CGPoint(x: self.cameraView.frame.width / 2, y: self.cameraView.frame.height / 2)
+                            layer.bounds = self.cameraView.frame
+                            
+                        }
+                        
+                    }
+                    
+                }catch {
+                    
+                    
+                    
+                }
+                
+                
+            }
+            
+        }
+
+    
+    }
+    
+    
 
     @IBAction func takephoto_pressed(sender: UIButton) {
         
